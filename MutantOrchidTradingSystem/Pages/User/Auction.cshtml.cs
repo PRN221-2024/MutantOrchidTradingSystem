7d4bd679fe9d81330c23a28323f51802d30a2347
Kha Cao <kha15cm@gmail.com>
2024-03-11 04:13:45 +0700

    feat: UI UX Auction

---
+++ MutantOrchidTradingSystem/Pages/User/Auction.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MutantOrchidTradingSysRazorPage.Pages.User
+{
+    public class AuctionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MutantOrchidTradingSystem/Pages/User/Bid.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MutantOrchidTradingSysRazorPage.Pages.User
+{
+    public class BidModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
